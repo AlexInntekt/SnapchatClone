cbc0f884b7fd1c5f997bce6e84b2e8125fd0e1a7
Manolescu Mihai Alexandru <alex.inntekt@gmail.com>
2017-08-08 23:09:34 +0300

    almost done.

--- SnapchatClone/ShowSnapVC.swift
+++ SnapchatClone/ShowSnapVC.swift
@@ -20,34 +20,27 @@ class ShowSnapVC: UIViewController
     
     @IBOutlet weak var snapImage: UIImageView!
 
+    @IBOutlet weak var snapTitle: UILabel!
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
 
-        let currentUid = Auth.auth().currentUser?.uid
         
-        Database.database().reference().child("Users").child(currentUid!).child("snaps").observe(DataEventType.childAdded, with: {(snap) in print(snap)
-            
-            let fetchedSnap = Snap()
-            
-            fetchedSnap.from = snap.childSnapshot(forPath: "from").value as! String
-            fetchedSnap.imageURL = snap.childSnapshot(forPath: "imageURL").value as! String
-            fetchedSnap.description = snap.childSnapshot(forPath: "description").value as! String
-            
-            
-            
-            if let adressURL = URL(string: fetchedSnap.imageURL)
-            {
-                self.snapImage.sd_setImage(with: adressURL, completed: nil)
-            }
-            else
-            {
-                print("\n\nProblem over here")
-            }
-            
-        })
+        print("\n > ", specificSnap)
         
+        if let adressURL = URL(string: specificSnap.imageURL)
+        {
+            self.snapImage.sd_setImage(with: adressURL, completed: nil)
+        }
+        else
+        {
+            print("\n\nProblem over here. ")
+            
+        }
+        
+        snapTitle.text = specificSnap.description
+
 
     }
 
--- SnapchatClone/SnapsVC.swift
+++ SnapchatClone/SnapsVC.swift
@@ -25,6 +25,22 @@ class SnapsVC: UIViewController, UITableViewDataSource, UITableViewDelegate
         snapsTableView.dataSource = self
         snapsTableView.delegate = self
         
+        let currentUid = Auth.auth().currentUser?.uid
+        
+        Database.database().reference().child("Users").child(currentUid!).child("snaps").observe(DataEventType.childAdded, with: {(snap) in print()
+            
+            let fetchedSnap = Snap()
+            
+            fetchedSnap.from = snap.childSnapshot(forPath: "from").value as! String
+            fetchedSnap.imageURL = snap.childSnapshot(forPath: "imageURL").value as! String
+            fetchedSnap.description = snap.childSnapshot(forPath: "description").value as! String
+            
+            self.currentSnaps.append(fetchedSnap)
+            self.snapsTableView.reloadData()
+            
+        })
+
+
 
     }
 
@@ -45,7 +61,7 @@ class SnapsVC: UIViewController, UITableViewDataSource, UITableViewDelegate
     
     @IBAction func addSnap(_ sender: Any)
     {
-  
+        performSegue(withIdentifier: "addButtonSegue", sender: nil)
     }
     
     //this function hides the status bar upwards:
@@ -54,29 +70,33 @@ class SnapsVC: UIViewController, UITableViewDataSource, UITableViewDelegate
         return true
     }
     
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
         return currentSnaps.count
     }
     
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
         let cell = UITableViewCell()
-        
-        
+        cell.textLabel?.text = "\(currentSnaps[indexPath.row].from): \(currentSnaps[indexPath.row].description)"
         return cell
     }
+    
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
     {
         performSegue(withIdentifier: "showSnapSegue", sender: currentSnaps[indexPath.row])
+        print("\n Trying to send by segue: ", currentSnaps[indexPath.row].description)
     }
+    
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?)
     {
         if(segue.identifier=="showSnapSegue")
         {
-            let nextVC = ShowSnapVC()
+            let nextVC = segue.destination as! ShowSnapVC
             nextVC.specificSnap = sender as! Snap
         }
     }
